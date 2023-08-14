7b40dafce23ba00ff8cbc1e9d22df20d4cd24117
lballi01 <97550612+lballi01@users.noreply.github.com>
2023-08-15 00:22:34 +0100

    Add files via upload

---
+++ MVVM/Model/ServerModel.cs
@@ -0,0 +1,16 @@
+﻿namespace VPN.MVVM.ViewModel
+{
+    public class ServerModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Server{ get; set; }
+
+        public string Country { get; set; }
+
+
+    }
+}
